ba969d7d60a17bc2c529fe3ba1dd5f19fb5d4a22
Shamil <badalloffsh@gmail.com>
2018-08-14 15:35:46 +0200

    Add: font-awesome and main css styles

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # my gems
 gem 'jquery-rails', '~> 4.3.3'
 gem 'bootstrap-sass', '~> 3.3.7'
+gem 'font-awesome-sass', '~> 5.2.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,26 @@
 class WelcomeController < ApplicationController
+  layout "landing"
+
   def index
+    @last_projects = last_projects
+  end
+
+  private
+
+  def last_projects
+    [
+        {
+            title: 'Практический семинар на тему " Особенности трудоустройста в Азербайджане и зарубежом."',
+            img: "268x150.png"
+        },
+        {
+            title: 'Müştəriyə yönləndirilmiş xidmətin göstərilməsi və çarpaz satışın əsasları',
+            img: "268x150.png"
+        },
+        {
+            title: 'Практический семинар на тему " Особенности трудоустройста в Азербайджане и зарубежом."',
+            img: "268x150.png"
+        }
+    ]
   end
 end
