051cc03738ffa9fb98da2fb8baae4af2036a3cfe
Shamil <badalloffsh@gmail.com>
2018-09-23 02:22:52 +0200

    Added settings to deploy app

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Things you may want to cover:
 
 * Ruby version
 
-2.4.1
+2.5.1
 * System dependencies
 
 * Configuration
---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+root = "/opt/www/callista/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log" # логи ошибок
+stdout_path "#{root}/log/unicorn.log" # логи сервака
+listen "/tmp/unicorn.callista.sock"
+worker_processes 1
+timeout 30
