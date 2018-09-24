57a34152afdbed64de0f58bdac578eda89b24537
Shamil <badalloffsh@gmail.com>
2018-09-24 15:24:31 +0200

    Fix: name misspel in translation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,9 +9,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = t('contacts.thank_you_for_your_message_we_will_contact_you_soon')
+      flash.now[:notice] = t('contact.thank_you_for_your_message_we_will_contact_you_soon')
     else
-      flash.now[:error] = t('contacts.cannot_send_message')
+      flash.now[:error] = t('contact.cannot_send_message')
       render :new
     end
   end
