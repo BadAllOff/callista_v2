9f14e426ea36aeae14a3c4604361e35a0b2b4f52
Shamil <badalloffsh@gmail.com>
2018-09-30 18:36:29 +0200

    Add: feature test, service create

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
   factory :user do
     email 'user@example.com'
     password '123456789'
+    after(:build)   { |u| u.skip_confirmation_notification! }
+    after(:create)  { |u| u.confirm }
   end
 end
