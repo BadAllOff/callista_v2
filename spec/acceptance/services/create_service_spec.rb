ac7bfecfe74dd08824025fb77112095752ed9444
Shamil <badalloffsh@gmail.com>
2018-09-30 18:36:59 +0200

    --amend

--- spec/acceptance/services/create_service_spec.rb
+++ spec/acceptance/services/create_service_spec.rb
@@ -20,14 +20,9 @@ I want be able to add service
       fill_in I18n.t('activerecord.attributes.service.title'), with: 'Test service'
       fill_in I18n.t('activerecord.attributes.service.preview'), with: 'Test preview'
       fill_in I18n.t('activerecord.attributes.service.description'), with: 'Test description'
-      # click_on I18n.t('activerecord.attributes.service.preview_img')
-      # save_and_open_page
       attach_file 'Preview image', "#{Rails.root}/spec/fixtures/service.jpg"
       click_on I18n.t('helpers.submit.service.create')
 
-
-
-      # expect(page).to have_content 'Question successfully created'
       expect(page).to have_content 'Test service'
       expect(page).to have_content 'Test description'
     end
